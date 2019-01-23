5d8d64935e6df871f0e071564f74030e39b2705c
Roman Heckendorf <info@romischart.de>
2019-01-23 22:05:44 +0100

    added integTest for custom config argument.

--- integTest/run.sh
+++ integTest/run.sh
@@ -10,7 +10,7 @@ node ./../bin/ci.js https://immowelt.de -r > output.txt
 node ./validate.js
 
 # Integration Test for report generation without custom configuration loading
-# node ./../bin/ci.js https://immowelt.de -r > output.txt --config ci-config.json
+node ./../bin/ci.js https://immowelt.de -r > output.txt --config ci-config.json
 node ./validate.js
 
 cd -
